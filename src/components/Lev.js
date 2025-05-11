0238ed10a918d5cf68ddd2b93a842081c949fe7d
Лев <115982484+leokey47@users.noreply.github.com>
2025-05-11 12:58:02 +0300

    Создание компонента Lev

---
+++ src/components/Lev.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Lev() {
+  return (
+    <div className="lev-container">
+      <h1>Привет, я Lev!</h1>
+      <p>Добро пожаловать на мою страницу</p>
+    </div>
+  );
+}
+
+export default Lev;
